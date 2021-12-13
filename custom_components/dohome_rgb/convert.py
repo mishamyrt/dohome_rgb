499ac8064b4c8251fa362d29d7fd03b398b0b11c
mishamyrt <misha@myrt.co>
2021-12-13 21:39:45 +0400

    Add doc

--- custom_components/dohome_rgb/convert.py
+++ custom_components/dohome_rgb/convert.py
@@ -1,5 +1,7 @@
 def _dohome_percent(value):
+    """Converts dohome value (0-5000) to percent (0-1)"""
     return value / 5000
 
 def _dohome_to_uint8(value):
+    """Converts dohome value (0-5000) to uint8 (0-255)"""
     return 255 * _dohome_percent(value)
