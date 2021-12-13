ffb3b1d2500ca1b0e35076cf6e2f5695c280161d
mishamyrt <misha@myrt.co>
2021-12-14 00:00:15 +0400

    Improve convert module

--- custom_components/dohome_rgb/convert.py
+++ custom_components/dohome_rgb/convert.py
@@ -1,7 +1,13 @@
-def _dohome_percent(value):
-    """Converts dohome value (0-5000) to percent (0-1)"""
-    return value / 5000
+"""Converters for DoHome number format."""
+from __future__ import annotations
 
-def _dohome_to_uint8(value):
-    """Converts dohome value (0-5000) to uint8 (0-255)"""
-    return 255 * _dohome_percent(value)
+def _dohome_percent(value: int) -> int:
+    """Convert dohome value (0-5000) to percent (0-1)."""
+    return int(value / 5000)
+
+def _dohome_to_uint8(value: int) -> int:
+    """Convert dohome value (0-5000) to uint8 (0-255)."""
+    return int(255 * _dohome_percent(value))
+
+def _uint8_to_dohome(value: int) -> int:
+    return int(5000 * (value / 255))
